e8a9e8a2fe9c09eb45cb82125bb4287781a40cb9
Samet Aslan <sametaslan1710@gmail.com>
2024-07-14 17:28:50 +0300

    add: fetchData.jsx

---
+++ src/utils/fetchData.jsx
@@ -0,0 +1,16 @@
+//rapidapi
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': import.meta.env.REACT_APP_RAPID_API_KEY ,
+      'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
+
+export const fetchData = async (url, options)=>{
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data
+}
