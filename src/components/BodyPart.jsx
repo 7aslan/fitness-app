7c9615d53796fbdc95b5b55e24455312a5c4c826
Samet Aslan <sametaslan1710@gmail.com>
2024-07-14 20:26:15 +0300

    add: BodyPart.jsx for HorizontalScrollbar.jsx

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+import Icon from "../assets/icons/gym.png";
 
-const BodyPart = () => {
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
-    <div>BodyPart</div>
-  )
-}
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+        borderTop: bodyPart === item ? "4px solid #ff2625" : "",
 
-export default BodyPart
+        backgroundColor: "#fff",
+        borderBottomLeftRadius: "20px",
+        width: "270px",
+        height: "280px",
+        cursor: "pointer",
+        gap: "47px",
+      }}
+    >
+      <img
+        src={Icon}
+        alt="dumbell"
+        className="body-part-icon"
+        styles={{
+          width: "40px",
+          height: "40px",
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default BodyPart;
