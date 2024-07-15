26ef79f85be79bc453f7c957fef3c9e49b8b2a3e
Samet Aslan <sametaslan1710@gmail.com>
2024-07-15 21:19:23 +0300

    fetching: exercise data wrt its id.

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
+import { useParams } from "react-router-dom";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const ExerciseDetail = () => {
-  return (
-    <div>ExerciseDetail</div>
-  )
-}
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
 
-export default ExerciseDetail
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+    };
+    fetchExercisesData();
+  }, [id]);
+
+  return (
+    <Box>
+      <Detail />
+      <ExerciseVideos exerciseDetail={exerciseDetail} />
+      <SimilarExercises />
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
